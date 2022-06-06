6206c31c1794486531e1ad090f968beea641002f
Renaldo Abreu <renaldoc@ciandt.com>
2022-06-06 18:33:10 -0300

    introdução a testes

---
+++ 0x00/email/TesteEmail.java
@@ -0,0 +1,29 @@
+
+
+public class TesteEmail {
+
+    public static boolean testar_email_com_arroba(){
+        if (Pessoa.emailValid("email_teste@dominio.com.br")){
+            return true;
+        }
+        return false;
+
+    }
+
+    public static boolean testar_email_sem_arroba(){
+        if (Pessoa.emailValid("email_testedominio.com.br")){
+            return false;
+        }
+        return true;
+
+    }
+
+    public static boolean testar_email_mais_50_caracteres(){
+        if (Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br")){
+            return false;
+        }
+        return true;
+
+    }
+
+}
